98f74c4f8020cfa0ed7689ac17d8c3d487cc556e
Im Khem <im.khem@gmail.com>
2017-07-06 21:36:20 -0600

    Assignment Complete

--- src/customers.js
+++ src/customers.js
@@ -6,3 +6,20 @@ export const getCustomerList = function() {
     return response.data;
   })
 }
+
+export const getCustomer = function(id) {
+  return axios.get(apiURL+id).then(response => { return response.data; })
+}
+
+export const postCustomer = function(customer) {
+  return axios.post(apiURL, customer).then(response => { return response.data; })
+}
+
+export const updateCustomer = function(id, obj) {
+  return axios.patch(apiURL+id, obj).then(response => { return response.data; })
+}
+
+export const deleteCustomer = function(id) {
+  return axios.delete(apiURL+id).then(response => { return response.data; })
+}
+
